afe22e70e0c6a08eeb0fbdcb35febaae7b000121
Luiz Gabriel <lzg.lisboasantos5569@gmail.com>
2024-01-19 16:37:18 -0300

    updated

--- forca.py
+++ forca.py
@@ -3,17 +3,15 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
     
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ["_","_","_","_","_","_",]
-    
-    
-    enforcou = False
-    acertou = False
+    # palavra_secreta = "banana".upper()
+    # letras_acertadas = ["_","_","_","_","_","_",]
+    palavra_secreta = "maca".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     erros = 0
-    
     print(letras_acertadas)
-    
-    while(not enforcou and not acertou):
+
+    while("_" in letras_acertadas):
         chute = input("Digite uma letra: ")
         chute = chute.strip().upper()
         index = 0
@@ -25,14 +23,15 @@ def jogar():
                 index += 1
         else:
             erros += 1
-        enforcou = erros == 6
+            print(f"Erro! ainda restam {6 - erros} tentativas")
         
-        acertou = "_" not in letras_acertadas
-                
+        if erros == 6:
+            break
+                     
         print(letras_acertadas)
         letras_faltando = letras_acertadas.count('_')
         print(f"Ainda faltam {letras_faltando} letras a serem acertadas")
         
-    print("Parabéns! Você ganhou!") if acertou else print("Fim do jogo, você perdeu.")
+    print("Parabéns! Você ganhou!") if "_" not in letras_acertadas else print("Fim do jogo, você perdeu.")
 if(__name__ == "__main__"):
     jogar()
